a4324488a9c054eac8e2edcdbe47b8e2a4d42fd8
Andres Alvarez <andresmazo6@gmail.com>
2023-06-04 20:12:54 -0500

    sum.py file

---
+++ Taller1/sum.py
@@ -0,0 +1,12 @@
+
+def sum():
+    a = float(input('write your first number: ')) #First variable
+    b = float(input('write the second number: ')) #Second variable
+    result = a + b #Add both variables
+    print(f'la suma de {a} y {b} es {result}')
+    
+if __name__=='__main__':
+   sum()
+    
+
+
