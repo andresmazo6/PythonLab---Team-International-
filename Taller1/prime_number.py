61e872916864c5466960ecdf2f17f70eabe37d93
Andres Alvarez <andresmazo6@gmail.com>
2023-06-05 05:50:16 -0500

    adding prime_number.py

---
+++ Taller1/prime_number.py
@@ -0,0 +1,16 @@
+def is_prime(number):
+    if number < 2:
+        return False
+    for i in range(2, int(number ** 0.5) + 1):
+        if number % i == 0:
+            return False
+    return True
+
+
+user_input = int(input("Enter a number: ")) # Take user input
+
+
+if is_prime(user_input):  # Check if the number is prime
+    print(f"{user_input} is a prime number.")
+else:
+    print(f"{user_input} is not a prime number.")
